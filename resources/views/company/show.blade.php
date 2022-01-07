cbb912b336e3d1b00e76edf6e1afc53277f55a5e
Miloš Petković <elitasoft.web@gmail.com>
2022-01-08 00:24:05 +0100

    * New css class for company's function buttons.

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -10,15 +10,15 @@
 
             <div class="panel-body">
 
-                <a href="{{ action('App\Http\Controllers\HomeController@index') }}" class="btn btn-sm btn-default"><i class="glyphicon glyphicon-home"></i> Početna strana</a>
+                <a href="{{ action('App\Http\Controllers\HomeController@index') }}" class="btn btn-sm btn-default mb-2"><i class="glyphicon glyphicon-home"></i> Početna strana</a>
 
-                <a href="{{ url()->previous() }}" class="btn btn-sm btn-default mrg-l-5 mrg-r-5"><i class="glyphicon glyphicon-circle-arrow-left"></i> Nazad</a>
+                <a href="{{ url()->previous() }}" class="btn btn-sm btn-default mrg-l-5 mrg-r-5 mb-2"><i class="glyphicon glyphicon-circle-arrow-left"></i> Nazad</a>
 
-                <a href="/company/show/{{ $company->id }}/worker/create" class="btn btn-sm btn-primary m-r">Dodaj radnika</a>
+                <a href="/company/show/{{ $company->id }}/worker/create" class="btn btn-sm btn-primary m-r mb-2">Dodaj radnika</a>
 
-                <a href="{{ action('App\Http\Controllers\Company\CompanyController@edit', [ 'id' => $company->id]) }}" class="btn btn-sm btn-warning mrg-l-5">Ažuriraj komitenta</a>
+                <a href="{{ action('App\Http\Controllers\Company\CompanyController@edit', [ 'id' => $company->id]) }}" class="btn btn-sm btn-warning mrg-l-5 mb-2">Ažuriraj komitenta</a>
 
-                <a onclick="if (!confirm('Da li zaista želite da obrišete komitenta?! Brisanjem ćete obrisati i radnike iz ove kompanije!')) return false;"  href="{{ action('App\Http\Controllers\Company\CompanyController@delete', ['id' => $company->id ]) }}" class="btn btn-sm btn-danger pull-right"><span class="fa fa-check"></span>Obriši komitenta</a>
+                <a onclick="if (!confirm('Da li zaista želite da obrišete komitenta?! Brisanjem ćete obrisati i radnike iz ove kompanije!')) return false;"  href="{{ action('App\Http\Controllers\Company\CompanyController@delete', ['id' => $company->id ]) }}" class="btn btn-sm btn-danger pull-right mb-2"><span class="fa fa-check"></span>Obriši komitenta</a>
 
             </div>
 
