5c5dee2b6580a470a214a5695d806250ea596b54
Miloš Petković <elitasoft.web@gmail.com>
2022-01-22 22:48:15 +0100

    Adjusting main vue template.

--- resources/views/app-vue.blade.php
+++ resources/views/app-vue.blade.php
@@ -1,30 +1,3 @@
-{{--<!DOCTYPE html>--}}
-{{--<html lang="en">--}}
-{{--<head>--}}
-{{--    <meta charset="UTF-8">--}}
-{{--    <meta name="viewport" content="width=device-width, initial-scale=1.0">--}}
-{{--    <meta http-equiv="X-UA-Compatible" content="ie=edge">--}}
-{{--    <title>Document</title>--}}
-{{--</head>--}}
-{{--<body>--}}
-{{--<div id="app">--}}
-{{--    <v-app app>--}}
-{{--        <v-container>--}}
-{{--            <v-btn color="primary">--}}
-{{--                Primary--}}
-{{--            </v-btn>--}}
-{{--            <v-btn color="secondary">--}}
-{{--                Secondary--}}
-{{--            </v-btn>--}}
-{{--            <v-btn color="error">--}}
-{{--                Error--}}
-{{--            </v-btn>--}}
-{{--        </v-container>--}}
-{{--    </v-app>--}}
-{{--</div>--}}
-{{--<script src="{{ asset('js/app.js') }}"></script>--}}
-{{--</body>--}}
-{{--</html>--}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -57,80 +30,96 @@
 </head>
 
 <body>
-{{--    <div id="app">--}}
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-        @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand glyphicon glyphicon-home" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="menu10" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu" aria-labelledby="menu10">
+                                <li role="presentation">
+                                    <a href="{{ url('/logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
                 </ul>
             </div>
-        @endif
-
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand glyphicon glyphicon-home" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="menu10" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu" aria-labelledby="menu10">
-                                    <li role="presentation">
-                                        <a href="{{ url('/logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <div class="container">
-            <div class="row">
-
-                @include('flash::message')
-
-                @yield('content')
-
-            </div>
         </div>
-{{--    </div>--}}
+    </nav>
+
+    <div class="container">
+        <div class="row">
+
+            @include('flash::message')
+
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+
+                    <div id="app">
+                        <v-app app>
+                            <v-container>
+                                <v-btn color="primary">
+                                    Primary
+                                </v-btn>
+                                <v-btn color="secondary">
+                                    Secondary
+                                </v-btn>
+                                <v-btn color="error">
+                                    Error
+                                </v-btn>
+                            </v-container>
+                        </v-app>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('/js/moment-timezone-with-data.min.js') }}"></script>
