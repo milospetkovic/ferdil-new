3d48a31b6bdad204d6ce7dc418f6ff2041a808d4
Miloš Petković <elitasoft.web@gmail.com>
2022-04-25 20:32:50 +0200

    Update

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,85 +1,57 @@
 <template>
 
+    <div class="card text-center">
+
+        <div class="card-header">
+            ~ Aplikacija za praćenje isteka ugovora radnika ~
+        </div>
+
+        <div class="card-body">
+
+            <div v-if="isUserAuthenticated">
+
+                <div class="my-2">
+                    <a class="btn btn-info" href="#nogo">Unos novog komitenta</a>
+                </div>
+
+                <div class="my-2">
+                    <a class="btn btn-info" href="#nogo">
+                        Lista komitenata
+                        <span class="badge bg-secondary" title="Ukupan broj komitenata">{{ userCustomersCount }}</span>
+                    </a>
+                </div>
+
+                <div class="my-2">
+                    <a class="btn btn-info" href="#nogo">
+                        Lista radnika
+                        <span class="badge bg-secondary" title="Ukupan broj i broj aktivnih radnika">
+                            {{ userTotalWorkersCount }} ({{ userActiveWorkersCount }})
+                        </span>
+                    </a>
+                </div>
+
+                <div class="my-2">
+                    <a class="btn btn-warning" href="#nogo">Pošalji notifikacije</a>
+                </div>
+
+                <div class="my-2">
+                    <a class="btn btn-warning" href="#nogo">Pokreni deaktivaciju radnika</a>
+                </div>
 
 
-        <div class="card text-center">
+                <div class="mt-5">
+                    <button class="btn btn-outline-warning" @click="logoutUser">Izloguj se</button>
+                </div>
 
-            <div class="card-header">
-                ~ Aplikacija za praćenje isticanje ugovora radnika ~
             </div>
 
-            <div class="card-body">
+            <div v-else>
 
-                <div v-if="isUserAuthenticated">
+                <h2>Morate da se ulogujete da biste imali pristup aplikaciji.</h2>
 
-                    <div class="my-2">
-                        <a class="btn btn-info" href="#nogo">Unos novog komitenta</a>
-                    </div>
+                <router-link to="/login" :class="'btn btn-primary'">Login</router-link>
 
-                    <div class="my-2">
-                        <a class="btn btn-info" href="#nogo">
-                            Lista komitenata
-                            <span class="badge bg-secondary" title="Ukupan broj komitenata">{{ userCustomersCount }}</span>
-                        </a>
-                    </div>
-
-                    <div class="my-2">
-                        <a class="btn btn-info" href="#nogo">
-                            Lista radnika
-                            <span class="badge bg-secondary" title="Ukupan broj i broj aktivnih radnika">
-                                {{ userTotalWorkersCount }} ({{ userActiveWorkersCount }})
-                            </span>
-                        </a>
-                    </div>
-
-                    <div class="my-2">
-                        <a class="btn btn-warning" href="#nogo">Pošalji notifikacije</a>
-                    </div>
-
-                    <div class="my-2">
-                        <a class="btn btn-warning" href="#nogo">Pokreni deaktivaciju radnika</a>
-                    </div>
-
-
-                    <div class="mt-5">
-                        <button class="btn btn-outline-warning" @click="logoutUser">Izloguj se</button>
-                    </div>
-
-
-<!--                    <h1>Welcome {{ loggedUser.name }}</h1>-->
-
-<!--                    <div class="text-center">-->
-<!--                        <button class="btn btn-primary" @click="logoutUser">Logout</button>-->
-<!--                    </div>-->
-
-<!--                    <div class="text-center my-2">-->
-<!--                        <button type="button" class="btn btn-primary" @click="testApi">Test API (Logged!)</button>-->
-<!--                    </div>-->
-
-<!--                    <div class="companies">-->
-<!--                        <template v-if="this.$store.getters.getUserCustomers">-->
-<!--                            <ul v-for="(customer, index) in this.$store.getters.getUserCustomers">-->
-<!--                                <li>{{ customer.id }} - {{ customer.name }}</li>-->
-<!--                            </ul>-->
-<!--                        </template>-->
-<!--                        <template v-else>-->
-<!--                            <div class="text-warning">Nemate klijente.</div>-->
-<!--                        </template>-->
-<!--                    </div>-->
-
-                </div>
-
-                <div v-else>
-
-                    <h2>Morate da se ulogujete da biste imali pristup aplikaciji.</h2>
-
-                    <router-link to="/login" :class="'btn btn-primary'">Login</router-link>
-
-<!--                    <div class="text-center my-2">-->
-<!--                        <button type="button" class="btn btn-primary" @click="testApi">Test API (not logged)</button>-->
-<!--                    </div>-->
-
-                </div>
+            </div>
 
 <!--                <div class="row mrg-t-10 text-center clearfix">-->
 <!--                    <a class="btn btn-info" href={{ action('App\Http\Controllers\Company\CompanyController@create') }}>Unos komitenata</a>-->
@@ -106,24 +78,23 @@
 <!--                        <small>{{ vcs_info(false) }}</small>-->
 <!--                    </p>-->
 <!--                </div>-->
-            </div>
-
-            <div class="card-footer text-muted">
-                <small>Show app version.</small>
-                <!--                    <small>{{ vcs_info(false) }}</small>-->
-            </div>
-
         </div>
+
+        <div class="card-footer text-muted">
+            <small>Show app version.</small>
+            <!--                    <small>{{ vcs_info(false) }}</small>-->
+        </div>
+
+    </div>
 
 </template>
 <script>
     const axios = require('axios');
-    //import { VProgressCircular } from 'vuetify/lib'
 
     export default {
         name: 'Home',
         components: {
-          //VProgressCircular
+          //
         },
         data() {
             return {
